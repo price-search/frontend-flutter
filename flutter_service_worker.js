c0c80b5c4e5a02bb05743311a474cda46136fc9d
Felipe Santos <felipecassiors@gmail.com>
2020-02-19 03:08:38 +0000

    publish: chore(deploy): add script to deploy to Github Pages and Surge
    generated from commit aae50f272f6bebd1b9f760b415b1d7c0c851a028

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,16 +1,16 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+  "/main.dart.js": "41066c43b06e985269386e7a2356a89c",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets/LICENSE": "1a80be6c5724a31e6f9c9e06dba53b63",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/index.html": "17d6d2f56486900bc9cb33e729f1feb1",
 "/manifest.json": "a58ec2a5d141fd5365bc201c0456a69c",
-"/main.dart.js": "41066c43b06e985269386e7a2356a89c"
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };
 
 self.addEventListener('activate', function (event) {
