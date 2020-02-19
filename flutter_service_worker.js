295fcd280b01e08063f8b2e68a69366810670133
Felipe Santos <felipecassiors@gmail.com>
2020-02-19 23:58:34 +0000

    publish: fix( folder): change folder name to inglesh
    generated from commit 9669092667e405175153dceed936471154162458

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,18 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/index.html": "17d6d2f56486900bc9cb33e729f1feb1",
-"/manifest.json": "a58ec2a5d141fd5365bc201c0456a69c",
+  "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/assets/FontManifest.json": "f7161631e25fbd47f3180eae84053a51",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/LICENSE": "be799750435a54a1480b4210a8c0f1c0",
 "/assets/AssetManifest.json": "1db22a9bb1aae68e807aff4988fc1be9",
-"/assets/lib/images/price-search.svg": "c45443991b409fcf748651b558d21657",
+"/assets/LICENSE": "be799750435a54a1480b4210a8c0f1c0",
+"/assets/FontManifest.json": "f7161631e25fbd47f3180eae84053a51",
 "/assets/lib/images/ps.png": "bdc6efb675f4f11761f858d9e9f3cfe9",
+"/assets/lib/images/price-search.svg": "c45443991b409fcf748651b558d21657",
+"/manifest.json": "a58ec2a5d141fd5365bc201c0456a69c",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/main.dart.js": "af4bc945229d39ae0d3067bfc69744ac"
+"/main.dart.js": "e3428741f4d990e16aa9284692b8f6ce",
+"/index.html": "17d6d2f56486900bc9cb33e729f1feb1"
 };
 
 self.addEventListener('activate', function (event) {
