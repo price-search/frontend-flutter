ddbbbe684675571f8f03e4063f5ede9d5e3cef24
luccorrea <correa.lucas1994@gmail.com>
2020-02-19 03:35:26 +0000

    publish: fiz estabelecimentos locas page
    generated from commit 59c913057608f069b0c759b204df06c174fe7f32

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,16 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/main.dart.js": "41066c43b06e985269386e7a2356a89c",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/LICENSE": "1a80be6c5724a31e6f9c9e06dba53b63",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/index.html": "17d6d2f56486900bc9cb33e729f1feb1",
+  "/index.html": "17d6d2f56486900bc9cb33e729f1feb1",
 "/manifest.json": "a58ec2a5d141fd5365bc201c0456a69c",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/FontManifest.json": "f7161631e25fbd47f3180eae84053a51",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/LICENSE": "be799750435a54a1480b4210a8c0f1c0",
+"/assets/AssetManifest.json": "1db22a9bb1aae68e807aff4988fc1be9",
+"/assets/lib/images/price-search.svg": "c45443991b409fcf748651b558d21657",
+"/assets/lib/images/ps.png": "bdc6efb675f4f11761f858d9e9f3cfe9",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/main.dart.js": "af4bc945229d39ae0d3067bfc69744ac"
 };
 
 self.addEventListener('activate', function (event) {
